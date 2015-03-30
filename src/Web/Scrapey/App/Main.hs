3d2f50e11fecfdf33009bd38ef5aa1fe8d5c37d6
Levi Notik <levinotik@gmail.com>
2015-03-30 12:27:06 -0400

    Add ToJSON instance for Tweet. Remove product hunt scraping

--- src/Web/Scrapey/App/Main.hs
+++ src/Web/Scrapey/App/Main.hs
@@ -3,19 +3,14 @@
 module Web.Scrapey.App.Main where
 
 import Text.HTML.Scalpel
-import Data.Text (Text, pack, unpack)
-import qualified  Data.Text.Lazy as LT
+import Data.Text (Text, pack)
 import qualified Text.StringLike()
 import Data.Monoid
 import qualified Web.Scotty as WS
-import Data.Functor
+import Data.Functor()
 import Control.Monad.IO.Class (liftIO)
-import Control.Monad (liftM)
-import Data.Maybe (fromMaybe)
 import Web.Scrapey
-import Data.Aeson
-import Control.Applicative (pure)
-import Network.HTTP.Types (Status, StdMethod, status404)
+import Network.HTTP.Types (status404)
 
 
 
@@ -24,43 +19,10 @@ main = WS.scotty 3000 $ do
      WS.get "/pagetitle" $ do
           url <- WS.param "url"
           liftIO (pageTitle url) >>= maybe (WS.status status404) WS.json
+     WS.get "/tweet" $ do
+          url <- WS.param "url"
+          liftIO (tweet url) >>= maybe (WS.status status404) WS.json
 
-
-test = do
-  t <- tweet "https://twitter.com/LeviNotik/status/581526803631775744"
-  putStrLn (show t)
-
-f = PageTitle "foo" "bar"
-
-type HuntDescription = Text
-type HuntLink = Text
-type HuntTitle = Text
-
-data Hunt = Hunt HuntTitle HuntDescription HuntLink
- deriving Show
-
-type TwitterHandle = Text
-type TweetText = Text
-
-data Tweet = Tweet TwitterHandle TweetText
-  deriving Show
-
--- instance WS.ToJSON Tweet where
-
-
-
-allHunts :: IO (Maybe [Hunt])
-allHunts = scrapeURL "http://www.producthunt.com" hunts
-   where
-       hunts :: Scraper Text [Hunt]
-       hunts = chroots (pack "div" @: [hasClass "url"]) huntDescriptions
-
-       huntDescriptions :: Scraper Text Hunt
-       huntDescriptions = do
-           title      <- text $ pack "a" @: [hasClass "title"]
-           desc      <- text $ pack "span" @: [hasClass "description"]
-           link      <-  attr "href" $ pack "a"
-           return $ Hunt title desc  (pack "http://www.producthunt.com" <> link)
 
 pageTitle :: String -> IO (Maybe PageTitle)
 pageTitle url = scrapeURL url title
@@ -78,6 +40,6 @@ tweet url = scrapeURL url getTweet
 
     innerTweet :: Scraper Text Tweet
     innerTweet = do
-      handle <- text $ pack "span" @: [hasClass "username"]
+      screenName <- text $ pack "span" @: [hasClass "username"]
       tweetText <- text $ pack "p" @: [hasClass "tweet-text"]
-      return $ Tweet handle tweetText
+      return $ Tweet screenName tweetText
--- src/Web/Scrapey/Types.hs
+++ src/Web/Scrapey/Types.hs
@@ -10,9 +10,25 @@ type Title = Text
 type Url = Text
 
 data PageTitle = PageTitle Title Url
+  deriving (Show, Eq)
 
 instance ToJSON PageTitle where
      toJSON (PageTitle title url) = object ["title" .= title, "url" .= url]
 
 
 
+type HuntDescription = Text
+type HuntLink = Text
+type HuntTitle = Text
+
+data Hunt = Hunt HuntTitle HuntDescription HuntLink
+  deriving (Show, Eq)
+
+type ScreenName = Text
+type TweetText = Text
+
+data Tweet = Tweet ScreenName TweetText
+  deriving (Show, Eq)
+
+instance ToJSON Tweet where
+     toJSON (Tweet screenName tweetText) = object ["screenName" .= screenName, "tweetText" .= tweetText]

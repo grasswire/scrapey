ee63b2fa66995279d58ded247f65b0a3cb8b0cee
Levi Notik <levinotik@gmail.com>
2015-03-30 16:52:33 -0400

    Remove test code

--- src/Main.hs
+++ src/Main.hs
@@ -6,17 +6,4 @@ import Web.Scrapey.APIs
 import System.Environment
 
 main :: IO()
-main =   ScrapeyApp.main --putStrLn (show $ ScrapeyApp.parse ScrapeyApp.twitterStatusUrl "https://twitter.com/BothsidesTV/status/579339241416577024")
-
--- testConfig :: IO()
---
--- testConfig = do
---   config <- load [Required "/Users/levinotik/haskell/scrapey/test.cfg"]
---   credentials <- twitterCreds config
---   oauth <- twitterAuth config
---   t <- lookupTweet "582609392974966784" oauth credentials
---   putStrLn (show t)
-
-
-
-
+main =   ScrapeyApp.main
